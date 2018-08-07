f4f0a663fa4737a6ac99769189c8667495f51d74
Roy Spekreijse <roy@spekreijse.eu>
2018-08-08 00:56:38 +0200

    added setFavorite

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 use Illuminate\Http\Request;
 
 /*
@@ -20,8 +22,20 @@ Route::get('/restaurant', function( Request $request) {
     $restaurants = DB::table('restaurants') 
         ->join('sorting_values', 'restaurants.id', '=', 'sorting_values.restaurant_id')
         ->get();    
-    return $restaurants;
-    
+    return $restaurants;    
     //return $request->restaurant();
+});
+Route::post('/restaurant', function(Request $request) {
+    $restaurant_name = $request->name; 
+    $update = DB::table('restaurants')->where('name', '=', $restaurant_name)    
+    ->update([
+        "isFavorite" => $request->isFavorite
+    ]);
+    if ($update){
+        return response()->json(["success" => true]);
+    } else {
+        return reponse()->abort(404);
+    }
+    
 });
 
